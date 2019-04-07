3c2c4b3f290d21b42be4e56e2feeec52e614bf7f
TheTiriRiri <kopciu12@gmail.com>
2019-04-07 12:20:54 +0200

    servlet dependency added 2

--- src/main/java/ShowServlet.java
+++ src/main/java/ShowServlet.java
@@ -1,2 +1,12 @@
-public class ShowServlet{
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
+
 }
