9f30f7bd99af80637a8034e2f0c543faece80014
TheTiriRiri <kopciu12@gmail.com>
2019-04-07 12:32:01 +0200

    added showError

--- src/main/java/ShowServlet.java
+++ src/main/java/ShowServlet.java
@@ -9,4 +9,19 @@ import java.io.IOException;
 @WebServlet("/show")
 public class ShowServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String[] hobbies = req.getParameterValues("hobby");
+        String lastName = req.getParameter("lastName");
+
+        HttpSession session = req.getSession();
+
+
+        if (hobbies.length == 0) {
+            req.getRequestDispatcher("showError.jsp").forward(req, resp);
+        }
+
+        req.getRequestDispatcher("show.jsp").forward(req, resp);
+    }
 }
---
+++ src/main/webapp/showError.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: konra
+  Date: 07.04.2019
+  Time: 12:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<div>
+<%="Error"%>
+</label></div>
+</body>
+</html>
